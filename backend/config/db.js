657ad6bd049cd5a8e41ab355ad25a0afe09238ee
Vikraam <vikraam17@gmail.com>
2026-06-20 12:30:10 +0530

    Database connected in mongodb atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/todo");
+        await mongoose.connect(
+  "mongodb://viky:viky@ac-pkpfemz-shard-00-00.hxhzkss.mongodb.net:27017,ac-pkpfemz-shard-00-01.hxhzkss.mongodb.net:27017,ac-pkpfemz-shard-00-02.hxhzkss.mongodb.net:27017/?ssl=true&replicaSet=atlas-t20m0q-shard-0&authSource=admin&appName=KPRCASB1"
+);
         console.log("MongoDB Connected Successfully🎉")
     }
     catch(err){
